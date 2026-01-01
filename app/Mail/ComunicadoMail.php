edb3455cf444b05565d4e4f24178f3aed27886de
brunoxzr <kaybruno177@gmail.com>
2026-01-01 17:59:19 -0300

    fix: prod

--- app/Mail/ComunicadoMail.php
+++ app/Mail/ComunicadoMail.php
@@ -1,16 +1,31 @@
 <?php
+
 namespace App\Mail;
 
 use App\Models\Comunicado;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
 
-class ComunicadoMail extends Mailable
+class ComunicadoMail extends Mailable implements ShouldQueue
 {
-    public function __construct(public Comunicado $comunicado) {}
+    use Queueable, SerializesModels;
+
+    public Comunicado $comunicado;
+
+    public function __construct(Comunicado $comunicado)
+    {
+        $this->comunicado = $comunicado;
+    }
 
     public function build()
     {
         return $this
+            ->from(
+                config('mail.from.address'),
+                config('mail.from.name')
+            )
             ->subject('📢 Comunicado - ' . $this->comunicado->titulo)
             ->view('emails.comunicado');
     }
